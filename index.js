d5a698abca2b089504614ef611ac32b00fad951c
etc1208 <599068284@qq.com>
2016-05-21 16:54:37 +0800

    finish function

--- index.js
+++ index.js
@@ -17,23 +17,63 @@ var PRODUCTS = [
 ];
 
 var FilterableProductTable = React.createClass({
+	getInitialState: function () {
+		return {
+			filterText: '',
+			inStockOnly: false
+		};
+	},
+
+	handleUserInput: function(filterText, inStockOnly) {
+		this.setState({
+			filterText: filterText,
+			inStockOnly: inStockOnly
+		});
+	},
+
 	render: function() {
 		return (
 			<div>
-				<SearchBar />
-				<ProductTable products={this.props.products} />
+				<SearchBar 
+					filterText={this.state.filterText}
+					inStockOnly={this.state.inStockOnly}
+					onUserInput={this.handleUserInput}
+				/>
+				<ProductTable 
+					products={this.props.products} 
+					filterText={this.state.filterText}
+					inStockOnly ={this.state.inStockOnly}
+				/>
 			</div>
 		);
 	}
 });
 
 var SearchBar = React.createClass({
+	handleChange: function() {
+		this.props.onUserInput(
+			this.refs.filterTextInput.value,
+			this.refs.inStockOnlyInput.checked
+		);
+	},
+
 	render: function() {
 		return (
 			<form>
-				<input type='text' placeholder='Search...'/>
+				<input 
+					type='text' 
+					placeholder='Search...' 
+					value={this.props.filterText} 
+					ref='filterTextInput'
+					onChange={this.handleChange}
+				/>
 				<p>
-					<input type='checkbox' />
+					<input 
+						type='checkbox' 
+						checked={this.props.inStockOnly}
+						ref='inStockOnlyInput'
+						onChange={this.handleChange}
+					/>
 					{' '}
 					Only show products in stock
 				</p>
@@ -47,12 +87,16 @@ var ProductTable = React.createClass({
 		var rows = [];
 		var lastCategory = null;
 		this.props.products.forEach(function(product) {
+			
+			if(product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+				return;
+			}
 			if(product.category !== lastCategory) {
 				rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
 			}
 			rows.push(<ProductRow product={product} key={product.name} />);
 			lastCategory = product.category;
-		});
+		}.bind(this));
 		return (
 			<table>
 				<thead>
@@ -95,9 +139,3 @@ ReactDOM.render(
 	<FilterableProductTable products={PRODUCTS} />,
 	document.getElementById('container')
 );
-
-
-
-
-
-
